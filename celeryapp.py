7e747b38ecb4a8591fa7ddf493deab694de6f7c2
Mayur Patel <mayurpatel.py@gmail.com>
2024-11-12 19:33:40 +0530

    schedule task removed

--- celeryapp.py
+++ celeryapp.py
@@ -2,25 +2,23 @@ from __future__ import absolute_import
 from celery import Celery
 from celery.schedules import crontab
 
-app = Celery('reco-client-backend-scrapy',include=['tasks','schedule_tasks'])
+
+app = Celery('reco-client-backend-scrapy',include=['tasks'])
 app.config_from_object("celeryconfig")
 app.conf.update(
     result_expires=3600,
 )
 app.conf.beat_schedule = {
     'schedule-reco-scrapy-download-every-3-minute': {
-        'task': 'schedule_tasks.schedule_scrapy_download_worker',
+        'task': 'tasks.schedule_scrapy_download_worker',
         'schedule': crontab(minute='*/3'),
     },
     'schedule-reco-scrapy-status-every-5-minute': {
-        'task': 'schedule_tasks.schedule_scrapy_status_worker',
+        'task': 'tasks.schedule_scrapy_status_worker',
         'schedule': crontab(minute='*/5'),
     },
     'schedule-reco-scrapy-requests-every-5-minute': {
-        'task': 'schedule_tasks.schedule_scrapy_requests_worker',
+        'task': 'tasks.schedule_scrapy_requests_worker',
         'schedule': crontab(minute='*/5'),
     }
 }
-
-# if __name__ == '__main__':
-#     app.start()

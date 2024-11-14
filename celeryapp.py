209586a63496ca6e2f8f26f6336ed8e933eb1e5e
Mayur Patel <mayurpatel.py@gmail.com>
2024-11-14 14:23:26 +0530

    celery config added

--- celeryapp.py
+++ celeryapp.py
@@ -8,17 +8,17 @@ app.config_from_object("celeryconfig")
 app.conf.update(
     result_expires=3600,
 )
-app.conf.beat_schedule = {
-    'schedule-reco-scrapy-download-every-3-minute': {
-        'task': 'tasks.schedule_scrapy_download_worker',
-        'schedule': crontab(minute='*/3'),
-    },
-    'schedule-reco-scrapy-status-every-5-minute': {
-        'task': 'tasks.schedule_scrapy_status_worker',
-        'schedule': crontab(minute='*/5'),
-    },
-    'schedule-reco-scrapy-requests-every-5-minute': {
-        'task': 'tasks.schedule_scrapy_requests_worker',
-        'schedule': crontab(minute='*/5'),
-    }
-}
+# app.conf.beat_schedule = {
+#     'schedule-reco-scrapy-download-every-3-minute': {
+#         'task': 'tasks.schedule_scrapy_download_worker',
+#         'schedule': crontab(minute='*/3'),
+#     },
+#     'schedule-reco-scrapy-status-every-5-minute': {
+#         'task': 'tasks.schedule_scrapy_status_worker',
+#         'schedule': crontab(minute='*/5'),
+#     },
+#     'schedule-reco-scrapy-requests-every-5-minute': {
+#         'task': 'tasks.schedule_scrapy_requests_worker',
+#         'schedule': crontab(minute='*/5'),
+#     }
+# }
--- celeryconfig.py
+++ celeryconfig.py
@@ -8,12 +8,28 @@ broker_transport_options = {'region': 'ap-south-1', 'visibility_timeout': 43200,
 mongodb_backend_settings = {
     "taskmeta_collection": 'task_back_request_job'
 }
+
 broker_url = os.getenv('CELERY_BROKER_URL')
 result_backend = os.getenv('CELERY_RESULT_BACKEND')
 task_default_queue = "local" # No Use of this as we are using queue name while pusing
 task_time_limit = 3600
 task_soft_time_limit = 3600
 result_backend_always_retry = True
+
+beat_schedule = {
+    'schedule-reco-scrapy-download-every-3-minute': {
+        'task': 'tasks.schedule_scrapy_download_worker',
+        'schedule': crontab(minute='*/3'),
+    },
+    'schedule-reco-scrapy-status-every-5-minute': {
+        'task': 'tasks.schedule_scrapy_status_worker',
+        'schedule': crontab(minute='*/5'),
+    },
+    'schedule-reco-scrapy-requests-every-5-minute': {
+        'task': 'tasks.schedule_scrapy_requests_worker',
+        'schedule': crontab(minute='*/5'),
+    }
+}
 
 # this ensures that the worker acks the task after it’s completed. If the worker crashes, it will just restart.
 task_acks_late = True
@@ -25,4 +41,4 @@ worker_prefetch_multiplier = 1
 # If you have it set to True, whenever you call delay or apply_async it will just run the task synchronously instead
 # of delegating it to a worker. This simplifies debugging in your local environment and facilitates automated testing.
 task_always_eager = False
-
+imports = ['tasks']

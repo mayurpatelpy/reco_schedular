b42a0b2f30bd441e9d6f96f47fb3c80358bf0b53
Mayur Patel <mayurpatel.py@gmail.com>
2025-01-09 14:47:39 +0530

    crown time and delay time chnage for ajio

--- celeryconfig.py
+++ celeryconfig.py
@@ -20,17 +20,17 @@ result_backend_always_retry = True
 beat_schedule = {
     'schedule-reco-scrapy-download-every-3-minute': {
         'task': 'tasks.schedule_scrapy_worker',
-        'schedule': crontab(minute='*/5'),
+        'schedule': crontab(minute='*/3'),
         'args': ('DOWNLOAD',)
     },
     'schedule-reco-scrapy-status-every-5-minute': {
         'task': 'tasks.schedule_scrapy_worker',
-        'schedule': crontab(minute='*/8'),
+        'schedule': crontab(minute='*/5'),
         'args': ('STATUS',)
     },
     'schedule-reco-scrapy-requests-every-5-minute': {
         'task': 'tasks.schedule_scrapy_worker',
-        'schedule': crontab(minute='*/8'),
+        'schedule': crontab(minute='*/5'),
         'args': ('REQUEST',)
     },
     'schedule-reco-amazon-daily-entry': {
